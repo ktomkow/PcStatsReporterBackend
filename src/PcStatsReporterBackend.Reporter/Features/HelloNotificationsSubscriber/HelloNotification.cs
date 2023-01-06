362476048233a4b069fd9139317ddcb23ed3df50
ktomkow <krzysztof.tomkow@gmail.com>
2023-01-07 00:36:51 +0100

    Add mediatr test

---
+++ src/PcStatsReporterBackend.Reporter/Features/HelloNotificationsSubscriber/HelloNotification.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace PcStatsReporterBackend.Reporter.Features.HelloNotificationsSubscriber;
+
+public class HelloNotification : INotification
+{
+    public string Message { get; set; }
+}
---
+++ src/PcStatsReporterBackend.Reporter/Features/HelloNotificationsSubscriber/HelloNotificationTwo.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace PcStatsReporterBackend.Reporter.Features.HelloNotificationsSubscriber;
+
+public class HelloNotificationTwo :  INotificationHandler<HelloNotification>
+{
+    private readonly ILogger<HelloNotificationTwo> _logger;
+
+    public HelloNotificationTwo(ILogger<HelloNotificationTwo> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(HelloNotification notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Handler {Handler} got message: {Message}", nameof(HelloNotificationTwo), notification.Message);
+        return Task.CompletedTask;
+    }
+}
