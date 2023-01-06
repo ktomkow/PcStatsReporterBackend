f766fe303e75d76296c2b7d30ab390ba7260e2fa
ktomkow <krzysztof.tomkow@gmail.com>
2023-01-06 19:24:37 +0100

    Add and map signalR

---
+++ src/PcStatsReporterBackend.AspNet/SignalR/MainHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PcStatsReporterBackend.AspNet.SignalR;
+
+/// <summary>
+/// Main signalr hub to talk to clients
+/// </summary>
+public class MainHub : Hub
+{
+    public override Task OnConnectedAsync()
+    {
+        return base.OnConnectedAsync();
+    }
+}
