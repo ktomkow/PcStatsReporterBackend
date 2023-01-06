8075db1255b1a253c3866b8776b33aec0cc759c5
ktomkow <krzysztof.tomkow@gmail.com>
2023-01-06 23:28:10 +0100

    Create reporter project with sample hosted service

---
+++ src/PcStatsReporterBackend.Reporter/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PcStatsReporterBackend.Reporter;
+
+public class Program
+{
+    public static async Task<int> Main()
+    {
+        var hostBuilder = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<HelloWorldService>();
+
+                services.AddHttpClient();
+            });
+        
+        await hostBuilder.RunConsoleAsync();
+            
+        return Environment.ExitCode;
+    }
+}
