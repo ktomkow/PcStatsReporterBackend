4490fdb4891b5b1a830ce858cea2c25309fab8bb
ktomkow <krzysztof.tomkow@gmail.com>
2023-01-06 23:28:10 +0100

    Create reporter project with sample hosted service

---
+++ src/PcStatsReporterBackend.Reporter/HelloWorldService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace PcStatsReporterBackend.Reporter;
+
+public class HelloWorldService : IHostedService
+{
+    private readonly ILogger<HelloWorldService> _logger;
+
+    public HelloWorldService(ILogger<HelloWorldService> logger)
+    {
+        _logger = logger;
+    }
+    
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            _logger.LogInformation("Hello");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
