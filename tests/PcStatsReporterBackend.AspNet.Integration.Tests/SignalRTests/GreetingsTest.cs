6c74439703cb56d309473f64e1ca0a3cae987f79
ktomkow <krzysztof.tomkow@gmail.com>
2023-01-06 21:02:02 +0100

    First signalr test that make sense

--- tests/PcStatsReporterBackend.AspNet.Integration.Tests/SignalRTests/GreetingsTest.cs
+++ tests/PcStatsReporterBackend.AspNet.Integration.Tests/SignalRTests/GreetingsTest.cs
@@ -1,4 +1,7 @@
-﻿namespace PcStatsReporterBackend.AspNet.Integration.Tests.SignalRTests;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using PcStatsReporterBackend.Contracts;
+
+namespace PcStatsReporterBackend.AspNet.Integration.Tests.SignalRTests;
 
 public class GreetingsTest : IntegrationTest
 {
@@ -14,12 +17,20 @@ public class GreetingsTest : IntegrationTest
         await mainHubConnection.StartAsync();
     }
     
-    
     [Fact]
-    public async Task StartConnection_ShouldWork2()
+    public async Task StartConnection_WhenConnectionStarted_GreetingShouldBeExecutedOnClient()
     {
         var mainHubConnection = _mainHubConnection;
+        ServerWelcome response = new ();
+        
+        mainHubConnection.On<ServerWelcome>("greeting", (serverWelcome) =>
+        {
+            response = serverWelcome;
+        });
 
         await mainHubConnection.StartAsync();
+
+        response.Should().NotBeNull();
+        response.Message.Should().Be("Hello");
     }
 }
