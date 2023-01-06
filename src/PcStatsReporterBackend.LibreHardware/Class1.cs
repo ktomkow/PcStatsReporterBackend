db4ceb41bda4dfdc50125df66cc4673969d9422b
ktomkow <krzysztof.tomkow@gmail.com>
2023-01-06 18:33:53 +0100

    Just a little bit of projects and configurations

---
+++ src/PcStatsReporterBackend.LibreHardware/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace PcStatsReporterBackend.LibreHardware;
+
+public class Class1
+{
+}
---
+++ tests/PcStatsReporterBackend.Core.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+
+namespace PcStatsReporterBackend.Core.Unit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        true.Should().BeTrue();
+    }
+}
