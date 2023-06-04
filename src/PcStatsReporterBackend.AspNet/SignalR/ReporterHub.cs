7dcdf59d7fe1af9e3e8352186f8cb04a497c7615
ktomkow <krzysztof.tomkow@gmail.com>
2023-06-04 14:19:11 +0200

    Work in progress

---
+++ src/PcStatsReporterBackend.AspNet/SignalR/ReporterHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using PcStatsReporterBackend.Contracts;
+
+namespace PcStatsReporterBackend.AspNet.SignalR;
+
+/// <summary>
+/// Hub that collects data
+/// </summary>
+public class ReporterHub : Hub
+{
+    private readonly ILogger<ReporterHub> _logger;
+
+    public ReporterHub(ILogger<ReporterHub> logger)
+    {
+        _logger = logger;
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        ServerWelcome serverWelcome = new()
+        {
+            Message = "Hello"
+        };
+
+        var userIdentifier = Context.UserIdentifier;
+        var connectionId = Context.ConnectionId;
+        
+        
+        _logger.LogInformation("User identifier: {Identifier}, connection identifier: {Connection}", userIdentifier, connectionId);
+        await Clients.Caller.SendCoreAsync("greeting", new object?[]{serverWelcome});
+    }
+}
